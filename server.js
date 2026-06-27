8d3c980c62e0ff004c65872c2a1e00db5605c7b0
ka-be-lo <sepitleal@gmail.com>
2026-06-27 18:11:20 +0200

    Fix CORS - use origin true instead of wildcard

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // --- Middleware ---
 app.use(helmet({ contentSecurityPolicy: false }));
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
